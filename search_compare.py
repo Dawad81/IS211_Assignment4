d402cf0d9ea0ba38656fe1e8ca39c2ed4a31698c
Dawad81 <dennis.awad83@spsmail.cuny.edu>
2019-02-23 17:27:04 -0500

    Working code.

--- search_compare.py
+++ search_compare.py
@@ -4,9 +4,11 @@
 
 
 import time
+import random
 
 
 def sequential_search(a_list, item):
+    """Docstring."""
     start = time.time()
     pos = 0
     found = False
@@ -15,16 +17,17 @@ def sequential_search(a_list, item):
             found = True
         else:
             pos = pos+1
-
     end = time.time()
     return found, end - start
 
-test_list = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-print(sequential_search(test_list, 3))
-print(sequential_search(test_list, 13))
+test_list1 = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+print sequential_search(test_list1, -1)
+print sequential_search(test_list1, -1)
 
 
 def ordered_sequential_search(a_list, item):
+    """Docstring."""
+    a_list.sort()
     start = time.time()
     pos = 0
     found = False
@@ -39,13 +42,15 @@ def ordered_sequential_search(a_list, item):
                 pos = pos+1
     end = time.time()
     return found, end - start
-      
-test_list = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-print(ordered_sequential_search(test_list, 3))
-print(ordered_sequential_search(test_list, 13))
+
+test_list2 = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print ordered_sequential_search(test_list2, -1)
+print ordered_sequential_search(test_list2, -1)
 
 
 def binary_search_iterative(a_list, item):
+    """Docstring."""
+    a_list.sort()
     start = time.time()
     first = 0
     last = len(a_list) - 1
@@ -61,31 +66,73 @@ def binary_search_iterative(a_list, item):
                 first = midpoint + 1
     end = time.time()
     return found, end - start
-      
-test_list = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-print(binary_search_iterative(test_list, 3))
-print(binary_search_iterative(test_list, 13))
+
+test_list3 = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print binary_search_iterative(test_list3, -1)
+print binary_search_iterative(test_list3, -1)
 
 
 def binary_search_recursive(a_list, item):
+    """Docstring."""
+    a_list.sort()
     start = time.time()
     if len(a_list) == 0:
         end = time.time()
-        return False, end - start
+        found = False #, end - start
     else:
         midpoint = len(a_list) // 2
         if a_list[midpoint] == item:
             end = time.time()
-            return True, end - start
+            found = True #, end - start
         else:
             if item < a_list[midpoint]:
                 return binary_search_recursive(a_list[:midpoint], item)
             else:
                 return binary_search_recursive(a_list[midpoint + 1:], item)
-   
-    #return found, end - start
 
-test_list = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-print(binary_search_recursive(test_list, 3))
-print(binary_search_recursive(test_list, 13))
+    return found, end - start
 
+test_list4 = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print binary_search_recursive(test_list4, -1)
+print binary_search_recursive(test_list4, -1)
+
+
+def get_me_random_list(number):
+    """Generate list of n elements in random order
+    :params: n: Number of elements in the list
+    :returns: A list with n elements in random order
+    """
+
+    r_list = range(number)
+    random.shuffle(r_list)
+    return r_list
+
+test_random = get_me_random_list(200)
+print test_random
+
+
+def main():
+    """Docstring."""
+    list_size = dict(list_size500=500, list_size1k=1000, list_size10k=10000)
+    for test_list in list_size.values():
+        r_list = get_me_random_list(test_list)
+        iter_count = 100
+        run_time_total = {'Sequential': 0,
+                          'Ordered_sequential': 0,
+                          'Binary_iterative': 0,
+                          'Binary_recursive': 0}
+        while iter_count > 0:
+            run_time_total['Sequential'] += sequential_search(r_list, -1)[1]
+            run_time_total['Ordered_sequential'] += ordered_sequential_search(r_list, -1)[1]
+            run_time_total['Binary_iterative'] += binary_search_iterative(r_list, -1)[1]
+            run_time_total['Binary_recursive'] += binary_search_recursive(r_list, -1)[1]
+            iter_count -= 1
+        print "List of %s random #'s:" % test_list
+        print "Sequential Search took %10.7f seconds to run on average." % float(run_time_total['Sequential'] / 100)
+        print "Ordered Sequential Search took %10.7f seconds to run on average." % float(run_time_total['Ordered_sequential'] / 100)
+        print "Binary Search Iterative took %10.7f seconds to run on average." % float(run_time_total['Binary_iterative'] / 100)
+        print "Binary Search Recursive took %10.7f seconds to run on average." % float(run_time_total['Binary_recursive'] / 100)
+
+
+if __name__ == '__main__':
+    main()
--- sort_compare.py
+++ sort_compare.py
@@ -2,11 +2,12 @@
 # -*- coding utf-8 -*-
 """Assignment 4 Part 2"""
 
-
+import random
 import time
 
 
 def insertion_sort(a_list):
+    """Docstring."""
     start = time.time()
     for index in range(1, len(a_list)):
         current_value = a_list[index]
@@ -15,29 +16,31 @@ def insertion_sort(a_list):
             a_list[position] = a_list[position - 1]
             position = position - 1
             a_list[position] = current_value
-            end = time.time()
+    end = time.time()
     return a_list, end - start
-    
+
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 test1 = insertion_sort(a_list)
-print(test1)
+print test1
 
 
 def shell_sort(a_list):
+    """Docstring."""
     start = time.time()
-    
+
     sublist_count = len(a_list) // 2
     while sublist_count > 0:
         for start_position in range(sublist_count):
             gap_insertion_sort(a_list, start_position, sublist_count)
-        print("After increments of size", sublist_count, "The list is", a_list)
+        #print("After increments of size", sublist_count, "The list is", a_list)
         sublist_count = sublist_count // 2
 
     end = time.time()
 
     return a_list, end - start
-    
+
 def gap_insertion_sort(a_list, start, gap):
+    """Docstring."""
     for i in range(start + gap, len(a_list), gap):
         current_value = a_list[i]
         position = i
@@ -46,13 +49,14 @@ def gap_insertion_sort(a_list, start, gap):
             position = position - gap
 
         a_list[position] = current_value
-      
+
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 test2 = shell_sort(a_list)
-print(test2)
+print test2
 
 
 def python_sort(input_list):
+    """Docstring."""
     start = time.time()
     input_list.sort()
     end = time.time()
@@ -60,6 +64,42 @@ def python_sort(input_list):
 
 a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 test3 = python_sort(a_list)
-print(test3)
-   
+print test3
 
+
+def get_me_random_list(number):
+    """Generate list of n elements in random order
+    :params: n: Number of elements in the list
+    :returns: A list with n elements in random order
+    """
+
+    r_list = range(number)
+    random.shuffle(r_list)
+    return r_list
+
+
+test_random = get_me_random_list(200)
+print test_random
+
+def main():
+    """Docstring."""
+    list_size = dict(list_size500=500, list_size1k=1000, list_size10k=10000)
+    for test_list in list_size.values():
+        r_list = get_me_random_list(test_list)
+        iter_count = 100
+        run_time_total = {'Insertion Sort': 0,
+                          'Shell Sort': 0,
+                          'Python Sort': 0}
+        while iter_count > 0:
+            run_time_total['Insertion Sort'] += insertion_sort(r_list)[1]
+            run_time_total['Shell Sort'] += shell_sort(r_list)[1]
+            run_time_total['Python Sort'] += python_sort(r_list)[1]
+            iter_count -= 1
+        print "List of %s random #'s:" % test_list
+        print "Insertion Sort took %10.7f seconds to run on average." % float(run_time_total['Insertion Sort'] / 100)
+        print "Shell Sort took %10.7f seconds to run on average." % float(run_time_total['Shell Sort'] / 100)
+        print "Python Sort took %10.7f seconds to run on average." % float(run_time_total['Python Sort'] / 100)
+
+
+if __name__ == '__main__':
+    main()
